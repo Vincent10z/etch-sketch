c5a21cc35d95cb11fec08a913d26835afce25e1f
Hexenmeistered <152895871+Hexenmeistered@users.noreply.github.com>
2024-03-07 12:12:51 +0200

    Add DOM catch

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
-const container = document.getElementById('container');
+document.addEventListener('DOMContentLoaded', function() {
+    // Your DOM manipulation code goes here...
+    const container = document.getElementById('container');
+    const button = document.getElementById('button');
+  })
 
-for (let i = 0; i < 16 * 16; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    container.appendChild(square);
+
+
+function getGridChoice(){
+    let rows = 16;
+    let col = 16;
+
 }
+
+function createGrid(rows, cols) {
+
+    for (let i = 0; i < rows * cols; i++) {
+      const square = document.createElement('div');
+      square.classList.add('square'); // Add the class 'box' to each div
+      container.appendChild(square); // Append each div to the container
+    }
+
+    square.addEventListener('mouseenter', () => {
+        square.classList.add('hovered');
+    });
+
+    square.addEventListener('mouseleave', () => {
+        square.classList.add('hovered');
+    });
+
+
+
+  }
+
+  createGrid(16, 16);
 
 
